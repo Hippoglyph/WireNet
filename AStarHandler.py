3b518e7828fc71108dd531181289d2cbb2bf0b71
Ponts <pbrink@kth.se>
2017-05-10 19:16:58 +0200

    Den löser inte omöjliga problem en

--- AStarHandler.py
+++ AStarHandler.py
@@ -1,4 +1,6 @@
 from circuit import Circuit
+import itertools
+import copy
 
 class Node:
     def __init__(self, value,row,col):
@@ -9,17 +11,56 @@ class Node:
         self.gcost = 0
         self.fcost = 0
 
+    
 class AStarHandler:
     def __init__(self, heuristic):
         self.circuit = Circuit()
+        self.circuit.start()
+        self.bestPerformer = copy.deepcopy(self.circuit)
         self.matrix = [[0 for x in range(self.circuit.ROWS)]for y in range(self.circuit.COLS)]
+        self.updateMatrix()
+        for wiresPermut in itertools.permutations(self.circuit.getWires(), len(self.circuit.getWires())):
+            #print(wiresPermut)
+            for wireKey in wiresPermut:
+                #print(wireKey)
+                wire = self.circuit.wires[wireKey]
+                start = self.matrix[wire.startR][wire.startC]
+                dest = self.matrix[wire.goalR][wire.goalC]
+                path = astar(start,dest,self.matrix, heuristic)
+                if len(path)<1:
+                    continue
+                current = path[0]
+                for item in path:
+                    move = item.row-current.row,item.col-current.col
+                    #print(move)
+                    #print(item.row, item.col)
+                    self.circuit.move(wireKey, move[0],move[1])
+                    current = item
+                self.updateMatrix()
+                #self.circuit.printPathMatrix()
+            #print("circuit fitness: ",self.circuit.getFitness()," Best performer fitness " ,self.bestPerformer.getFitness())
+            
+            if(int(self.circuit.getFitness())>int(self.bestPerformer.getFitness())):
+                #print("Updated best")
+                self.bestPerformer = copy.deepcopy(self.circuit)
+            self.circuit.restart()
+            self.updateMatrix()
+        #print("Best performer has fitness: ", self.bestPerformer.getFitness())
+        
+        self.bestPerformer.drawResult()
+        
+
+    def updateMatrix(self):
         for row in range(self.circuit.ROWS):
             for col in range(self.circuit.COLS):
-                self.matrix[row][col] = Node(self.circuit.pathM_[row][col],row,col)
-        for key in self.circuit.wirePoints:
-            path = astar(self.matrix[key[0]][key[1]],self.matrix[self.circuit.wirePoints[key][0]][self.circuit.wirePoints[key][1]],self.matrix, heuristic)
-        for item in path:
-            print(item.row, item.col)
+                self.matrix[row][col] = Node(self.circuit.pathM[row][col],row,col)
+                #print(self.matrix[row][col].value)
+        for wireKey in self.circuit.getWires():
+            wire = self.circuit.wires[wireKey]
+            #print(wire.goalR, wire.goalC)
+            self.matrix[wire.goalR][wire.goalC].value=2
+        
+            
 def manhattan(curr, dest):
     #print(abs(curr.row - dest.row) + abs(curr.col - dest.col))
     return  abs(curr.row - dest.row) + abs(curr.col - dest.col)
@@ -42,27 +83,36 @@ def neighbours(node,grid):
         
 
 def astar(start,dest,matrix, heuristic):
-    openSet = set()
-    closedSet = set()
+    #print(dest.row, dest.col)
+    #print(start)
+    openSet = []
+    closedSet = []
     current = start
     current.gcost = 0
     current.fcost=heuristic(current,dest)
-    openSet.add(current)
+    openSet.append(current)
     counter = 0
     while(openSet):
         counter += 1
         current = min(openSet, key=lambda c: c.fcost)
-        if current.row == dest.row and current.col == dest.col:
-            path = []
-            while current.parent:
+        for neg in neighbours(current, matrix):
+            if(dest.row == neg.row and dest.col == neg.col):
+                #print("Reached!!!")
+                path = []
+                dest.parent = current
+                current = dest
+                while current.parent:
+                    #print(current.row, current.col)
+                    path.append(current)
+                    current = current.parent
                 path.append(current)
-                current = current.parent
-            path.append(current)
-            print("Iterations: ", counter)
-            return path[::-1]
+                #print("Iterations: ", counter)
+                return path[::-1]
         openSet.remove(current)
-        closedSet.add(current)
+        closedSet.append(current)
         for node in neighbours(current,matrix):
+            if node.value == 2:
+                continue
             counter += 1
             if node in closedSet:
                 continue
@@ -75,17 +125,10 @@ def astar(start,dest,matrix, heuristic):
                 node.gcost = current.gcost + 1
                 node.fcost = node.gcost + heuristic(node,dest)
                 node.parent = current
-                openSet.add(node)
-            
+                openSet.append(node)
+    return []   
                                          
 
-#import tkinter as tk
-#root = tk.Tk()
-#root.wm_title("Normal Pathfinding")
-#buttonFrame = tk.Frame(root)
-#buttonFrame.grid(row=2, column=0, columnspan=2)
-#tk.Button(buttonFrame, text="AStar", command=lambda: AStarHandler(manhattan)).grid(row=0,column=1)
-#tk.Button(buttonFrame, text="BFS", command=lambda: AStarHandler(bfs)).grid(row=0,column=2)
-#buttonFrame.pack()            
-#root.update()
-#root.mainloop()
+
+#A = AStarHandler(manhattan)
+
